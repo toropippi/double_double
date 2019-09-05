8437b5615a116cfd01dedbf8be4760dd5c66af80
toropippi <44022497+toropippi@users.noreply.github.com>
2019-09-05 20:05:00 +0900

    pow関数実装

--- doubledouble.py
+++ doubledouble.py
@@ -46,7 +46,7 @@ def dd_mul(x,y):
     cz.lo = cz.lo + x.hi * y.lo + x.lo * y.hi + x.lo * y.lo
     return twosum(cz)
 
-# dd x
+# dd x : pow(x,2)
 def dd_pow2(x):
     xx = x.hi * x.hi
     ca = dsplit(x.hi)
@@ -79,16 +79,152 @@ def dd_sqrt(x):
         z2 = ((cz.hi + x.hi) + x.lo + cz.lo) / (2.0 * z1)
         return twosum(dd(z1, z2))
 
+# dd x,int y:xのpow(2,y)乗、yはべき指数であることに注意。y=0ならx,y=1ならxの2乗,y=-1ならyの平方根が出力
+def dd_powxpow2y(x,y):
+    xx = dd(x.hi,x.lo)
+    if y>=0:
+        for i in range(y):
+            xx=dd_pow2(xx)
+    else:
+        for i in range(-y):
+            xx=dd_sqrt(xx)
+    return xx
+
+#dd x,dd y
+def dd_pow(x,y):
+    yy = dd(y.hi,y.lo)
+    ans = dd(1.0, 0.0)
+    hisign = True
+    losign = True
+    if y.hi<0.0:
+        yy.hi = -yy.hi
+        hisign = False
+    if y.lo<0.0:
+        yy.lo = -yy.lo
+        losign = False
+    if yy.hi==0.0:#0乗
+        return ans
+    #ここでyyの仮数部のbitを取得したい
+    #まずyyを1含む～2含まずの範囲にscaleする、ここは仮数部をいじらない操作となる
+    scalee=0
+    if yy.hi>=1.0:
+        scalee=1024
+        for i in range(1024):
+            if yy.hi>=2.0:
+                yy.hi=yy.hi*0.5
+                yy.lo=yy.lo*0.5
+            else:
+                scalee = i
+                break
+        #これでscaleが求まった
+    else:#y.hi<1.0のとき
+        scalee=-1024
+        for i in range(1024):
+            if yy.hi<1.0:
+                yy.hi=yy.hi*2.0
+                yy.lo=yy.lo*2.0
+            else:
+                scalee = -i
+                break
+        #これでscaleが求まった
+    if scalee==-1024:
+        return ans
+    #この時点でyyは必ず1～2の範囲内にある(inf例外あり)
+    #ここから、何のくらいの桁がbitがたっているかを検出、リストに保存。ここでscale値正と負を分ける
+    pbitlisthi = []  # 中身は正のscalee値 hi
+    mbitlisthi = []  # 中身は負のscalee値 hi
+    pbitlistlo = []  # 中身は正のscalee値 lo
+    mbitlistlo = []  # 中身は負のscalee値 lo
+
+    for i in range(53):
+        if yy.hi>=1.0:
+            if scalee>=0:
+                pbitlisthi.append(scalee)
+            else:
+                mbitlisthi.append(scalee)
+            yy.hi-=1.0
+        yy.hi *= 2.0
+        yy.lo *= 2.0
+        scalee-=1
+    #次にloのことを考える
+    for i in range(2048):#擬似四倍精度特有の異常高精度を考慮し54ではなく2048
+        if yy.lo>=1.0:
+            if scalee>=0:
+                pbitlistlo.append(scalee)
+            else:
+                mbitlistlo.append(scalee)
+            yy.lo-=1.0
+        elif yy.lo==0.0:
+            break
+        yy.lo *= 2.0
+        scalee-=1
+    #ここで全部のビットとその符号が求まった。負の場合は逆数にしないといけない
+    #まずは小数点より上の指数を
+    pbitlistlo=pbitlistlo[::-1]
+    pbitlisthi=pbitlisthi[::-1]
+    #まずloから
+    mysc=0
+    lo_ans=dd(1.0, 0.0)
+    xp = dd(x.hi, x.lo)
+    for sc in pbitlistlo:
+        for i in range(sc-mysc):
+            xp=dd_pow2(xp)
+        lo_ans=dd_mul(lo_ans,xp)
+        mysc=sc
+    #次にhi
+    hi_ans=dd(1.0, 0.0)
+    for sc in pbitlisthi:
+        for i in range(sc-mysc):
+            xp=dd_pow2(xp)
+        hi_ans=dd_mul(hi_ans,xp)
+        mysc=sc
+
+    #次に小数点以下の指数を
+    #まずhi
+    xp = dd(x.hi, x.lo)
+    mysc=0
+    for sc in mbitlisthi:
+        for i in range(mysc-sc):
+            xp=dd_sqrt(xp)
+        hi_ans=dd_mul(hi_ans,xp)
+        mysc=sc
+    #次にlo
+    for sc in mbitlistlo:
+        for i in range(mysc-sc):
+            xp=dd_sqrt(xp)
+        lo_ans=dd_mul(lo_ans,xp)
+        mysc=sc
+
+    #最後に指数の符号を考慮しans
+    if losign:
+        if hisign:#++
+            ans = dd_mul(hi_ans, lo_ans)
+        else:#-+
+            ans = dd_div(lo_ans, hi_ans)
+    else:#+-
+        if hisign:
+            ans = dd_div(hi_ans, lo_ans)
+        else:#--
+            ans = dd_div(ans, dd_mul(hi_ans,lo_ans))
+    return ans
+
+
+
 
 
 if __name__ == '__main__':
-    dda=dd(1,0)
-    for i in range(53):
-        dda = dd_mul(dda, dd(2,0))#dd型で2の53乗を作る
+
+    dda=dd(2,0)
+    dda=dd_pow(dda,dd(53,0))
+    #for i in range(53):
+    #    dda = dd_mul(dda, dd(2,0))#dd型で2の53乗を作る
+    print("2の53乗のdd型の中身")
     print(dda.hi, dda.lo)
-    print()
+
 
     #dd型の9007199254740992に1を足していくループ。double型なら仮数部桁落ちでこれ以上増えないはず
+    print()
+    print("1ずつ足していく")
     for i in range(10):
         dda = dd_add(dda, dd(1,0))
         print(dda.hi,dda.lo)
